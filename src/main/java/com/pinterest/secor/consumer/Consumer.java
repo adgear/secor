9f1f0e4701c237109380bc981373d691d5e66dfb
David Haguenauer <david@adgear.com>
2016-02-02 16:26:16 -0500

    Ignore hopefully-transient HTTP errors.

--- src/main/java/com/pinterest/secor/consumer/Consumer.java
+++ src/main/java/com/pinterest/secor/consumer/Consumer.java
@@ -108,6 +108,9 @@ public class Consumer extends Thread {
     private void checkUploadPolicy() {
         try {
             mUploader.applyPolicy();
+        } catch(org.apache.http.NoHttpResponseException e) {
+            LOG.warn("Ignoring HTTP failure:");
+            e.printStackTrace();
         } catch (Exception e) {
             throw new RuntimeException("Failed to apply upload policy", e);
         }
